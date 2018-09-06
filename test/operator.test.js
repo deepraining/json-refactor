2b02ce4e9463e44b94b45af7c36ab2ac8b4c361e
senntyou <jiangjinbelief@163.com>
2018-09-06 15:15:10 +0800

    add test/operator.test.js

---
+++ test/operator.test.js
@@ -0,0 +1,86 @@
+const JSONRefactor = require('../dist/json-refactor');
+
+describe('Make an operator to original value.', () => {
+  test('built-in operators.', () => {
+    const target = { a: 1, b: '234', c: '1.22', d: '0.01', e: [{ value: 1 }, { value: 2 }] };
+    const mapping = {
+      aaa: 'a|bool',
+      bbb: 'b|int',
+      ccc: 'c|float',
+      ddd: 'd|int|bool',
+      sum: 'e|sum!value',
+      average: 'e|average!value',
+    };
+    const result = JSONRefactor(target, mapping);
+
+    expect(result).toBe(target);
+    expect(result.aaa).toBe(true);
+    expect(result.bbb).toBe(234);
+    expect(result.ccc).toBe(1.22);
+    expect(result.ddd).toBe(false);
+    expect(result.sum).toBe(3);
+    expect(result.average).toBe(1.5);
+    expect(Object.keys(result).length).toBe(11);
+  });
+
+  test('with dot semantic', () => {
+    const target = { a: { a: { a: 1 } } };
+    const mapping = { aaa: 'a.a.a|bool' };
+    const result = JSONRefactor(target, mapping);
+
+    expect(result).toBe(target);
+    expect(result.a.a.a).toBe(1);
+    expect(result.aaa).toBe(true);
+    expect(Object.keys(result).length).toBe(2);
+  });
+
+  test('register custom operators', () => {
+    JSONRefactor.register('double', value => (typeof value !== 'number' ? value : value * 2));
+    JSONRefactor.register({ test: 'triple', handler: value => (typeof value !== 'number' ? value : value * 3) });
+    JSONRefactor.register([
+      {
+        test: /^slice!/,
+        handler: (value, operator) => {
+          const operators = operator.split('!');
+          const start = parseInt(operators[1], 10);
+          const end = parseInt(operators[2], 10);
+          const hasStart = typeof start === 'number' && !isNaN(start);
+          const hasEnd = typeof end === 'number' && !isNaN(end);
+
+          let newValue = value;
+
+          if (hasStart && hasEnd) newValue = value.slice(start, end);
+          else if (hasStart) newValue = value.slice(start);
+
+          return newValue;
+        },
+      },
+    ]);
+
+    const target = { a: 1, b: 2, c: 'abcdefg' };
+    const mapping = { aaa: 'a|double', bbb: 'b|triple', abcd: 'c|slice!0!4', efg: 'c|slice!-3' };
+    const result = JSONRefactor(target, mapping);
+
+    expect(result).toBe(target);
+    expect(result.aaa).toBe(2);
+    expect(result.bbb).toBe(6);
+    expect(result.abcd).toBe('abcd');
+    expect(result.efg).toBe('efg');
+    expect(Object.keys(result).length).toBe(7);
+  });
+
+  test('custom operatorDelimiter by "||".', () => {
+    JSONRefactor.set({ operatorDelimiter: '||' });
+
+    const target = { a: 1, b: '234', c: '1.22', d: '0.01' };
+    const mapping = { aaa: 'a||bool', bbb: 'b||int', ccc: 'c||float', ddd: 'd||int||bool' };
+    const result = JSONRefactor(target, mapping);
+
+    JSONRefactor.set({ operatorDelimiter: '|' });
+
+    expect(result.aaa).toBe(true);
+    expect(result.bbb).toBe(234);
+    expect(result.ccc).toBe(1.22);
+    expect(result.ddd).toBe(false);
+  });
+});
